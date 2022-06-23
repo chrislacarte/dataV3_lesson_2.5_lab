2c3203b1a951c355967c7b4b570366a3ff17d2ca
Chris Lacarte <chrislacarte@icloud.com>
2022-06-23 13:18:44 +0100

    solved

--- Lab2.sql
+++ Lab2.sql
@@ -2,11 +2,11 @@ USE sakila;
 
 -- 1 Select all the actors with the first name ‘Scarlett’
 SELECT 
-    * 
+* 
 FROM
-    sakila.actor 
+sakila.actor 
 WHERE 
-    first_name LIKE 'Scarlett%';
+first_name LIKE 'Scarlett%';
 
 -- 2 How many films (movies) are available for rent  
 
@@ -15,72 +15,82 @@ WHERE
 
 -- if there is a dictinct id in inventory, means there is a movie to rent:
 SELECT 
-    COUNT(DISTINCT(film_id)) 
+COUNT(DISTINCT(film_id)) 
 FROM 
-    sakila.inventory;
+sakila.inventory;
 -- 2a how many films have been rented?
 -- If there is an id created on rental_id, has been rented, so we count how many rental_ids:
 SELECT 
-    COUNT(rental_id)
+COUNT(rental_id)
 FROM 
-    sakila.rental;
+sakila.rental;
 
 
 -- 3 What are the shortest and longest movie duration? Name the values max_duration and min_duration.
 -- We select by title and length and we want only the first value
+SELECT MIN(length)
+AS min_duration
+FROM sakila.film;
 
-SELECT *
-FROM 
-    (SELECT
-        'max_duration' AS '',
-        sakila.film.title,
-        sakila.film.length
-    FROM 
-        sakila.film
-    ORDER BY
-        sakila.film.length desc
-    LIMIT 1
-    ) AS T1
-UNION 
-SELECT * 
-FROM
-    (SELECT
-        'min_duration' AS '',
-        sakila.film.title AS min_duration,
-        sakila.film.length
-    FROM 
-        sakila.film
-    ORDER BY
-        sakila.film.length asc
-        LIMIT 1
-    ) AS T2;
+SELECT MAX(length)
+AS max_duration
+FROM sakila.film;
+
+
+
+-- SELECT *
+-- FROM 
+--     (SELECT
+--         'max_duration' AS '',
+--         sakila.film.title,
+--         sakila.film.length
+--     FROM 
+--         sakila.film
+--     ORDER BY
+--         sakila.film.length desc
+--     LIMIT 1
+--     ) AS T1
+-- UNION 
+-- SELECT * 
+-- FROM
+--     (SELECT
+--         'min_duration' AS '',
+--         sakila.film.title AS min_duration,
+--         sakila.film.length
+--     FROM 
+--         sakila.film
+--     ORDER BY
+--         sakila.film.length asc
+--         LIMIT 1
+--     ) AS T2;
     
     
 -- 4 What's the average movie duration expressed in format (hours, minutes)?
 
 SELECT 
-    convert(avg(length),time) as "hours:minutes" 
+convert(avg(length),time) as "hours:minutes" 
 FROM 
-    sakila.film;
+sakila.film;
 
 -- 5 How many distinct (different) actors' last names are there?
 SELECT 
-    COUNT(distinct(last_name))
+COUNT(distinct(last_name))
 FROM
-    actor;
+actor;
 
 -- 6 Since how many days has the company been operating (check DATEDIFF() function)? table rental , rent
 SELECT 
-    datediff(max(rental_date),min(rental_date)) AS days 
+datediff(max(rental_date),min(rental_date)) AS days 
 FROM 
-    sakila.rental;
+sakila.rental;
 
 -- 7 rental info with additional columns month and weekday. Get 20 results.
 SELECT 
-    *, 
-    date_format(CONVERT(left(rental_date,23),date), '%M') AS 'Month',
-    date_format(CONVERT(left(rental_date,23),date), '%W') AS 'Weekday'
-FROM sakila.rental 
+*, 
+date_format(CONVERT(left(rental_date,23),date), '%M') AS 'Month',
+date_format(CONVERT(left(rental_date,23),date), '%W') AS 'Weekday'
+FROM 
+sakila.rental 
 LIMIT 20;
 
 -- 8 Add an additional column day_type with values 'weekend' and 'workday' depending on the rental day of the week.
@@ -101,43 +111,44 @@ FROM sakila.rental LIMIT 20;
 
 -- 9 Get release years.
 SELECT
-    DISCTINCT(release_year)
+DISCTINCT(release_year)
 FROM 
-    sakila.film;
+sakila.film;
 
 
 -- 10 Get all films with ARMAGEDDON in the title.
 SELECT 
-    * 
+* 
 FROM 
-    sakila.film 
+sakila.film 
 WHERE 
-    title LIKE '%ARMAGEDDON%';
+title 
+LIKE '%ARMAGEDDON%';
 
 
 -- 11 Get all films which title ends with APOLLO.
 SELECT 
-    * 
+* 
 FROM 
-    sakila.film 
+sakila.film 
 WHERE 
-    title LIKE '%APOLLO';
+title LIKE '%APOLLO';
 
 
 -- 12 Get 10 the longest films.
 
 SELECT 
-    film.title, film.length 
+film.title, film.length 
 FROM 
-    sakila.film 
-ORDER BY length DESC LIMIT 10;
+sakila.film 
+ORDER BY length DESC LIMIT 20;
 
 -- 13 How many films include Behind the Scenes content?
 SELECT 
-    COUNT(*) 
+COUNT(*) 
 FROM 
-    sakila.film 
+sakila.film 
 WHERE 
-    special_features LIKE '%Behind the Scenes%';
+special_features LIKE '%Behind the Scenes%';
 
 
